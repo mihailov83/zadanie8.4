c863d27501c9139cecc61e3a466e978ce50d0959
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-17 00:44:53 +0200

    Add files via upload
    Add files to zadanie8.4

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// Prompt display dialog box for visitors
+alert('Hello, ' + name); 
+// Alert method display alert message
+console.log('Hello, ' + name);
+
+/* 
+first block comment
+*/
